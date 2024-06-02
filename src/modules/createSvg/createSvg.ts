90857fa441c869ed5c67c9a373dde59a18d46869
Kathrin <kathrin.ayer@gmail.com>
2024-06-02 10:09:51 -0400

    fit svg to output

--- src/modules/createSvg/createSvg.ts
+++ src/modules/createSvg/createSvg.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import parseGpml from './parseGpml';
 import errorProcessing from '../../utilities/errorProcessing';
 
-const svgHeader = `<svg width="3600" height="1800" xmlns="http://www.w3.org/2000/svg">`;
+const svgHeader = `<svg width="360" height="180" xmlns="http://www.w3.org/2000/svg">`;
 const svgFooter = '</svg>';
 
 async function createSvg(sourcePath: string, destPath: string, color?: string) {
