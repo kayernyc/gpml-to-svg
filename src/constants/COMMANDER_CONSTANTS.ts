a8b35730e3aae258d5bde0708719df37fd71301e
Kathrin <kathrin.ayer@gmail.com>
2024-07-24 08:39:25 -0400

    add new command group
    Added new command group to the index file. Extracted command option parameters to a constants file.

---
+++ src/constants/COMMANDER_CONSTANTS.ts
@@ -0,0 +1,20 @@
+export const TIME_DECLARATION = '-t, --time <number>';
+export const TIME_DESCRIPTION = 'point in time that a feature must exist';
+
+export const DESTINATION_DECLARATION = '-d, --destination <string>';
+export const DESTINATION_DESCRIPTION =
+  'point in time that a feature must exist';
+
+export const ROTATION_FILE_DECLARATION = '-r, --rotation-file <string>';
+export const ROTATION_FILE_DESCRIPTION = 'path to the rotation file';
+
+export const GENERATED_FILE_NAME_DECLARATION = '-fn, --file-name <string>';
+export const GENERATED_FILE_NAME_DESCRIPTION = 'name for generated file';
+
+export const MULTI_COLOR_DECLARATION = '-mc, --multi-color';
+export const MULTI_COLOR_DESCRIPTION =
+  'generate groups colored with variations on fill color';
+
+export const FILL_COLOR_DECLARATION = '-c, --color <string>';
+export const FILL_COLOR_DESCRIPTION = 'fill color';
+export const FILL_COLOR_DEFAULT = 'gray';
