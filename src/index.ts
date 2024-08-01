5c7bae2af971cd31cf528eba18d9336722de6428
Kathrin <kathrin.ayer@gmail.com>
2024-08-01 08:03:15 -0400

    convert to cli
    Added shebang. Chose gpsvg for cli name. Not a npm module or common cli

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,69 @@
-import { colorGradient } from "@commands/colorGradient";
-import { convert } from "@commands/convert";
-import { Command } from "commander";
-import {
-	BORDER_COLOR_DECLARATION,
-	BORDER_COLOR_DESCRIPTION,
-	DESTINATION_DECLARATION,
-	DESTINATION_DESCRIPTION,
-	FILL_COLOR_DECLARATION,
-	FILL_COLOR_DEFAULT,
-	FILL_COLOR_DESCRIPTION,
-	GENERATED_FILE_NAME_DECLARATION,
-	GENERATED_FILE_NAME_DESCRIPTION,
-	MULTI_COLOR_DECLARATION,
-	MULTI_COLOR_DESCRIPTION,
-	ROTATION_FILE_DECLARATION,
-	ROTATION_FILE_DESCRIPTION,
-	TIME_DECLARATION,
-	TIME_DESCRIPTION,
-} from "constants/COMMANDER_CONSTANTS";
+#! /usr/bin/env node
 
-import * as dotenv from "dotenv";
+import { colorGradient } from '@commands/colorGradient';
+import { convert } from '@commands/convert';
+import { Command } from 'commander';
+import {
+  BORDER_COLOR_DECLARATION,
+  BORDER_COLOR_DESCRIPTION,
+  DESTINATION_DECLARATION,
+  DESTINATION_DESCRIPTION,
+  FILL_COLOR_DECLARATION,
+  FILL_COLOR_DEFAULT,
+  FILL_COLOR_DESCRIPTION,
+  GENERATED_FILE_NAME_DECLARATION,
+  GENERATED_FILE_NAME_DESCRIPTION,
+  MULTI_COLOR_DECLARATION,
+  MULTI_COLOR_DESCRIPTION,
+  ROTATION_FILE_DECLARATION,
+  ROTATION_FILE_DESCRIPTION,
+  TIME_DECLARATION,
+  TIME_DESCRIPTION,
+} from 'constants/COMMANDER_CONSTANTS';
+
+import * as dotenv from 'dotenv';
 dotenv.config({ path: `${__dirname}/../.env` });
 
 const program = new Command();
 
-program.enablePositionalOptions();
-
-program.enablePositionalOptions().option("-p, --progress");
+program.enablePositionalOptions().option('-p, --progress');
 
 program
-	.version("0.0.1")
-	.description("A CLI for converting GPLates GPML files to SVGs.");
+  .version('0.0.1')
+  .description('A CLI for converting GPLates GPML files to SVGs.');
 
 program
-	.command("convert")
-	.requiredOption(TIME_DECLARATION, TIME_DESCRIPTION)
-	.requiredOption(DESTINATION_DECLARATION, DESTINATION_DESCRIPTION)
-	.option(FILL_COLOR_DECLARATION, FILL_COLOR_DESCRIPTION, FILL_COLOR_DEFAULT)
-	.option(ROTATION_FILE_DECLARATION, ROTATION_FILE_DESCRIPTION)
-	.option(GENERATED_FILE_NAME_DECLARATION, GENERATED_FILE_NAME_DESCRIPTION)
-	.option(MULTI_COLOR_DECLARATION, MULTI_COLOR_DESCRIPTION)
-	.option(BORDER_COLOR_DECLARATION, BORDER_COLOR_DESCRIPTION)
-	.argument("<filepaths...>")
-	.action(async (filepaths, options) => {
-		convert(filepaths, options);
-	});
+  .command('convert')
+  .requiredOption(TIME_DECLARATION, TIME_DESCRIPTION)
+  .requiredOption(DESTINATION_DECLARATION, DESTINATION_DESCRIPTION)
+  .option(FILL_COLOR_DECLARATION, FILL_COLOR_DESCRIPTION, FILL_COLOR_DEFAULT)
+  .option(ROTATION_FILE_DECLARATION, ROTATION_FILE_DESCRIPTION)
+  .option(GENERATED_FILE_NAME_DECLARATION, GENERATED_FILE_NAME_DESCRIPTION)
+  .option(MULTI_COLOR_DECLARATION, MULTI_COLOR_DESCRIPTION)
+  .option(BORDER_COLOR_DECLARATION, BORDER_COLOR_DESCRIPTION)
+  .argument('<filepaths...>')
+  .action(async (filepaths, options) => {
+    convert(filepaths, options);
+  });
 
 program
-	.command("color-gradient")
-	.requiredOption(TIME_DECLARATION, TIME_DESCRIPTION)
-	.requiredOption(DESTINATION_DECLARATION, DESTINATION_DESCRIPTION)
-	.requiredOption(
-		"-cr, --color-ramp <string>",
-		"file for gradient or color ramp",
-	)
-	.option(ROTATION_FILE_DECLARATION, ROTATION_FILE_DESCRIPTION)
-	.option(GENERATED_FILE_NAME_DECLARATION, GENERATED_FILE_NAME_DESCRIPTION)
-	.option(BORDER_COLOR_DECLARATION, BORDER_COLOR_DESCRIPTION)
-	.option("-f, --file-path <string>", "file to color with a gradient")
-	.action(async (options) => {
-		colorGradient(options);
-	});
+  .command('color-gradient')
+  .requiredOption(TIME_DECLARATION, TIME_DESCRIPTION)
+  .requiredOption(DESTINATION_DECLARATION, DESTINATION_DESCRIPTION)
+  .requiredOption(
+    '-cr, --color-ramp <string>',
+    'file for gradient or color ramp',
+  )
+  .option(ROTATION_FILE_DECLARATION, ROTATION_FILE_DESCRIPTION)
+  .option(GENERATED_FILE_NAME_DECLARATION, GENERATED_FILE_NAME_DESCRIPTION)
+  .option(BORDER_COLOR_DECLARATION, BORDER_COLOR_DESCRIPTION)
+  .option('-f, --file-path <string>', 'file to color with a gradient')
+  .action(async (options) => {
+    colorGradient(options);
+  });
 
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
