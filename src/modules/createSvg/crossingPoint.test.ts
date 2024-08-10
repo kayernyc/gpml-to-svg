3aa42d802126ba3b41fce706daca86008f4c2fa1
Kathrin <kathrin.ayer@gmail.com>
2024-08-10 09:07:00 -0400

    add testing for crossing point

--- src/modules/createSvg/crossingPoint.test.ts
+++ src/modules/createSvg/crossingPoint.test.ts
@@ -4,9 +4,15 @@ import { crossingPoint } from './crossingPoint';
 const latLonToCartesianCases = [
   {
     name: 'where 45, 45 generates [0.5, 0.5, 0.7071067811865475]',
-    previousPoint: { lat: 350, long: 45 },
+    previousPoint: { lat: 10, long: 45 },
     currentPoint: { lat: 10, long: -45 },
-    expected: 371.6666666666667,
+    expected: 10,
+  },
+  {
+    name: 'where 45, 45 generates [0.5, 0.5, 0.7071067811865475]',
+    previousPoint: { lat: 10, long: -45 },
+    currentPoint: { lat: 12, long: 45 },
+    expected: 7.666666666666666,
   },
 ];
 
